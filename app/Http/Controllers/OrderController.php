f4f882018efc699f67f41223006835767d203283
ksenia <lkettimail2005@gmail.com>
2024-10-26 12:27:00 +0300

    test2

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -2,9 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Order;
 use Illuminate\Http\Request;
+use Termwind\Components\Raw;
 
 class OrderController extends Controller
 {
-    //
+    public function index(){
+        Order::all();
+        return view('index', compact('orders'));
+    }
+
+    public function store(Request $request){
+
+    }
 }
